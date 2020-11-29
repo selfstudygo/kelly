ce46afd9663de39cb30a7b3f027a1631d2f942b6
kellywoo <kelly.kh.woo@gmail.com>
2020-11-29 21:33:17 +0900

    <content>: add 2 translation

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kelly/precache-manifest.15093bffe468531c3286e958ded935d2.js"
+  "/kelly/precache-manifest.d7ff66c18522054e548a194f47033217.js"
 );
 
 self.addEventListener('message', (event) => {
